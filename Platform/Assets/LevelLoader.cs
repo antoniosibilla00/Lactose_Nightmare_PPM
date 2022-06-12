d8cdefcd7ff3cd720029ce6d24a69ed01faf2278
antosib <gsibilla32@gmail.com>
2022-06-12 19:32:49 +0200

    adding pause menu
    adding pause menu

--- Platform/Assets/LevelLoader.cs
+++ Platform/Assets/LevelLoader.cs
@@ -1,14 +1,23 @@
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 public class LevelLoader : MonoBehaviour
 {
+    string[] insights = {"Il 90% dei nord Europei mantiene l’abilità di digerire il lattosio dopo l’infanzia contro il 5% degli Asiatici",
+        "Alcuni soggetti intolleranti accusano irritabilità e sbalzi d’umore", 
+        "Il valore soglia dell’intolleranza varia da persona a persona. In alcuni casi i sintomi sono talmente lievi da essere ignorati", 
+        "La probabilità di essere intolleranti sale per quella parte della popolazione la cui dieta normalmente esclude i latticini"};
 
+    
     public static LevelLoader instance;
+
+    [SerializeField] public Text insightsText1;
 
     [SerializeField] private GameObject loaderCanvas;
     [SerializeField] private Slider progressBar;
@@ -17,7 +26,7 @@ public class LevelLoader : MonoBehaviour
 
     private void Awake()
     {
-        
+        Debug.Log(instance);
         if(instance == null){
 
             instance = this;
@@ -40,15 +49,18 @@ public class LevelLoader : MonoBehaviour
         scene.allowSceneActivation = false ;
 
         loaderCanvas.SetActive(true);
-
+          
+        insightsText1.text =  insights[Random.Range(0, 4)]; 
+        
+        Debug.Log("sium");
         do{
 
-            await Task.Delay(100);
+            await Task.Delay(2000);
             target = scene.progress;
 
         }while(scene.progress < 0.9f);
 
-        await Task.Delay(1000);
+        await Task.Delay(3500);
 
         scene.allowSceneActivation = true ;
 
@@ -59,7 +71,9 @@ public class LevelLoader : MonoBehaviour
     void Update()
     {
         
-      progressBar.value = Mathf.MoveTowards(progressBar.value,target,3 * Time.deltaTime);
+        progressBar.value = Mathf.MoveTowards(progressBar.value,target,3 * Time.deltaTime);
+
+     
 
     }
 
